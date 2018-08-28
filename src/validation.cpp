cbe5baa4f5db5d291c6b8c0197eec6501fa5867f
amorecoin <amorecoin@hotmail.com>
2018-08-28 21:23:24 +0700

    Change subsidy for premine

--- src/validation.cpp
+++ src/validation.cpp
@@ -1046,6 +1046,10 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
         return 0;
 
     CAmount nSubsidy = 50 * COIN;
+    // For premine, get 30% from avaliable coin.
+    if (nHeight == 1) {
+        nSubsidy = 63000000 * COIN;
+    }
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
